4b8e4973d98308c50fc41faf5399be23f09239cc
Arthur Felipe <arthur.joaquim754@gmail.com>
2025-05-12 20:09:04 -0300

    atualização

---
+++ app.js
@@ -0,0 +1,13 @@
+function sortear(){
+    let quantidade = document.getElementById("quantidade").value;
+    let de = document.getElementById("de").value;
+    let ate = document.getElementById("ate").value;
+
+    //console.log (`Quantidade: ${quantidade} | De: ${de} | Até: ${ate}`);
+
+    
+}
+
+function reiniciar(){
+
+}
